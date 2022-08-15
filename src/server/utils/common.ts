1b52335e0ea134e22b0b301064d8da06cf7dd644
LuongTheLong <lrollking1@gmail.com>
2022-08-15 09:37:16 +0700

    +Add function convert Vietnamese to English

--- src/server/utils/common.ts
+++ src/server/utils/common.ts
@@ -1,5 +1,30 @@
-const slugGenerator = (title: string) => {
-  return title;
+const slugGenerator = (codeName: string) => {
+  codeName = codeName.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  codeName = codeName.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  codeName = codeName.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  codeName = codeName.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  codeName = codeName.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  codeName = codeName.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  codeName = codeName.replace(/đ/g, "d");
+  codeName = codeName.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+  codeName = codeName.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+  codeName = codeName.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+  codeName = codeName.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+  codeName = codeName.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+  codeName = codeName.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+  codeName = codeName.replace(/Đ/g, "D");
+  // Some system encode vietnamese combining accent as individual utf-8 characters
+  // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+  codeName = codeName.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+  codeName = codeName.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+  // Remove extra spaces
+  // Bỏ các khoảng trắng liền nhau
+  codeName = codeName.replace(/ + /g, " ");
+  codeName = codeName.trim();
+  // Remove punctuations
+  // Bỏ dấu câu, kí tự đặc biệt
+  codeName = codeName.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g, " ");
+  return codeName;
 };
 
 type NameGeneratorParams = {
@@ -7,6 +32,6 @@ type NameGeneratorParams = {
   age: number;
 };
 
-const nameGenerator = ({ name, age }: NameGeneratorParams) => {};
+const nameGenerator = ({ name, age }: NameGeneratorParams) => { };
 
 export { slugGenerator, nameGenerator };
