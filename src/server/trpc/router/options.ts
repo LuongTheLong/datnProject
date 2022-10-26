5791383a86ac939d4d2539f485c4e0641c166dc2
huylee99 <nhathuyle083@outlook.com>
2022-10-26 16:10:49 -0700

    option api integrated

---
+++ src/server/trpc/router/options.ts
@@ -0,0 +1,19 @@
+import { t } from "../_app";
+import { z } from "zod";
+
+export const optionsRouter = t.router({
+  getByCategory: t.procedure.input(z.object({ categoryId: z.string() })).query(async ({ ctx, input }) => {
+    const options = await ctx.prisma.optionCategory.findMany({
+      where: {
+        category: {
+          id: input.categoryId,
+        },
+      },
+      include: {
+        choices: true,
+      },
+    });
+
+    return options;
+  }),
+});
