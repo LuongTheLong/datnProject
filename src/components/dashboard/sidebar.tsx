4fa2941cecbd1f2b08680c438be7de22405dba68
huylee99 <nhathuyle083@outlook.com>
2022-12-05 23:55:34 -0800

    show menu

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -2,7 +2,7 @@ import { Flex, Heading, Box } from "@chakra-ui/react";
 import Link from "next/link";
 
 const MENUS = [
-  // { id: 1, slug: "", title: "Dashboard" },
+  { id: 1, slug: "", title: "Dashboard" },
   { id: 1, slug: "order", title: "Danh sách đơn hàng" },
   { id: 2, slug: "products", title: "Danh sách sản phẩm" },
   { id: 3, slug: "categories", title: "Danh sách danh mục" },
